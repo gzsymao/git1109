df742096381fa43d1bb40abd2d0b0308bc552b8b
Gzsymao <gzsymaoanyun@163.com>
2021-03-28 20:19:24 +0800

    hot-fix push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("master test!");
         System.out.println("master test!");
         System.out.println("master test2!");
+        System.out.println("github test!");
     }
 }
